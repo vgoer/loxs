7fc07d9c63d9263497196f11e9adf528c5218c87
vgoer <goerblue@gmail.com>
2024-12-30 13:41:07 +0800

    fix: 添加Subdomin脚本 收集子域名

--- subdomain.sh
+++ subdomain.sh
@@ -17,21 +17,6 @@ EOF
 # 输入主域名
 read -p "请输入主域名: " website_input
 
-# 显示进度条
-show_progress() {
-    local string=$1
-    local delay=0.1
-    local spinstr='|/-\'
-    while true; do
-        local temp=${spinstr#?}
-        printf " [%c] 正在${string}... " "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        printf "\r"
-    done
-    printf "\r\033[K"
-}
-
 
 # 创建输出目录
 if [ ! -d "$website_input" ]; then
@@ -44,14 +29,12 @@ fi
 echo "开始收集子域名..."
 # Subfinder扫描
 echo "[1/2] 使用Subfinder扫描..."
-(subfinder -d $website_input -all -recursive > $output_dir/subdomain1.txt) &
-show_progress $!
+subfinder -d $website_input -all -recursive > $website_input/subdomain1.txt
 echo "✓ Subfinder扫描完成"
 
 # crt扫描
 echo "[2/2] 使用CRT扫描..."
-(curl -s "https://crt.sh/?q=%.${website_input}&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $output_dir/subdomain2.txt) &
-show_progress $!
+curl -s "https://crt.sh/?q=%.${website_input}&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $website_input/subdomain2.txt
 echo "✓ 使用CRT扫描完成"
 
 
